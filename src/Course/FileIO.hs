eecfe80ddea2ec31221b9b0d33fa9f31904b4ebb
Chris Coffey <cmcof273@gmail.com>
2019-02-04 09:37:17 -0500

    Started state

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -39,7 +39,7 @@ Problem --
 
 Consideration --
   Try to avoid repetition. Factor out any common expressions.
-  
+
 Example --
 Given file files.txt, containing:
   a.txt
@@ -61,7 +61,7 @@ To test this module, load ghci in the root of the project directory, and do
 Example output:
 
 $ ghci
-GHCi, version ... 
+GHCi, version ...
 Loading package...
 Loading ...
 [ 1 of 28] Compiling (etc...
@@ -85,47 +85,56 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile path contents = do
+    putStrLn $ "File: " ++ path
+    putStrLn $ "Contents: " ++ contents
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles Nil = pure ()
+printFiles (a :. rest) = do
+    printPair a
+    printFiles rest
+    where
+        printPair (f,c) = printFile f c
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fp = do
+    contents <- readFile fp
+    pure (fp, contents)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles xs =
+    sequence $ getFile <$> xs
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run ff = do
+    (_, contents) <- getFile ff
+    let files = lines contents
+    printFiles =<< getFiles files
+
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
-
+main = do
+    args <- getArgs
+    void . sequence $ run <$> args
 ----
 
 -- Was there was some repetition in our solution?
